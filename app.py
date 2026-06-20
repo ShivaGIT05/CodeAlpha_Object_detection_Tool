88de155aa3d92b6de68c971f47086f777ff8321e
shiva kumar <shivagoudchedurupally@gmail.com>
2026-06-20 07:54:09 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,9 +1,38 @@
 from ultralytics import YOLO
 import cv2
+import pandas as pd
+import os
+from datetime import datetime
+import time
 
 model = YOLO("yolov8n.pt")
 
+os.makedirs(
+    "captures",
+    exist_ok=True
+)
+
+log_file = "detection_history.csv"
+
+if not os.path.exists(
+    log_file
+):
+
+    pd.DataFrame(
+        columns=[
+            "Time",
+            "Object"
+        ]
+    ).to_csv(
+        log_file,
+        index=False
+    )
+
 cap = cv2.VideoCapture(0)
+
+confidence = 0.6
+
+previous = time.time()
 
 while True:
 
@@ -12,19 +41,116 @@ while True:
     if not success:
         break
 
-    results = model.track(
+    results = model(
         frame,
-        persist=True
+        conf=confidence
     )
 
     output = results[0].plot()
 
+    counts = {}
+
+    for box in results[0].boxes:
+
+        cls = int(
+            box.cls
+        )
+
+        name = model.names[
+            cls
+        ]
+
+        counts[name] = (
+            counts.get(
+                name,
+                0
+            )
+            + 1
+        )
+
+    y = 40
+
+    for k,v in counts.items():
+
+        text = (
+            f"{k}: {v}"
+        )
+
+        cv2.putText(
+            output,
+            text,
+            (20,y),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.8,
+            (0,255,0),
+            2
+        )
+
+        y += 40
+
+        pd.DataFrame(
+            [
+                [
+                    datetime.now(),
+                    k
+                ]
+            ]
+        ).to_csv(
+            log_file,
+            mode="a",
+            header=False,
+            index=False
+        )
+
+    current = time.time()
+
+    fps = int(
+        1/(current-previous)
+    )
+
+    previous = current
+
+    cv2.putText(
+        output,
+        f"FPS: {fps}",
+        (20,400),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        0.8,
+        (255,0,0),
+        2
+    )
+
     cv2.imshow(
-        "Object Detection & Tracking",
+        "AI Object Detection",
         output
     )
 
-    if cv2.waitKey(1) == 27:
+    key = cv2.waitKey(1)
+
+    if key == ord("s"):
+
+        name = (
+            "captures/"
+            +
+            datetime.now().strftime(
+                "%H%M%S"
+            )
+            +
+            ".jpg"
+        )
+
+        cv2.imwrite(
+            name,
+            output
+        )
+
+        print(
+            "Saved:",
+            name
+        )
+
+    elif key == 27:
+
         break
 
 cap.release()
